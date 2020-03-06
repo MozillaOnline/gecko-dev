50045613eab9e10b9402cb5f94eae7a2eed0c16e
rxu <rxu@mozilla.com>
2020-03-06 11:15:06 +0800

    add google patch

--- mobile/android/base/java/org/mozilla/gecko/updater/UpdateServiceHelper.java
+++ mobile/android/base/java/org/mozilla/gecko/updater/UpdateServiceHelper.java
@@ -213,7 +213,7 @@ public class UpdateServiceHelper {
                 .replace("%MOZ_VERSION%", AppConstants.MOZILLA_VERSION);
 
         Log.i(LOGTAG, "AUS Url is: " + url);
-        String channelId = "base";
+        String channelId = "google";
 
         url = CN_UPDATE_URL
                 .replace("%LOCALE%", locale)
